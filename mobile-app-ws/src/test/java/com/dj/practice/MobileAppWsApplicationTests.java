12a9e6813e6fa798d6475188ed7b0c19bf29ffaf
DJDrama <putzzang@hotmail.com>
2021-02-02 14:20:47 +0900

    First Practice

---
+++ mobile-app-ws/src/test/java/com/dj/practice/MobileAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
