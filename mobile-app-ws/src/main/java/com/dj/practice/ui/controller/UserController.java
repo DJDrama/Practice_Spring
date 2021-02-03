b6eebbe8b89513ce02058d2f620c6249ad9088f8
DJDrama <putzzang@naver.com>
2021-02-03 14:17:40 +0900

    Post consumes, produces

--- mobile-app-ws/src/main/java/com/dj/practice/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/dj/practice/ui/controller/UserController.java
@@ -8,43 +8,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dj.practice.ui.model.request.UserDetailsRequestModel;
 import com.dj.practice.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
-	
+
 	@GetMapping
-	public String getUsers(@RequestParam(value="page", defaultValue="1") int page, 
-			@RequestParam(value="limit", defaultValue="50") int limit,
-			@RequestParam(value="sort", required=false) String sort) {
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "50") int limit,
+			@RequestParam(value = "sort", required = false) String sort) {
 		return "Get users was called! " + page + " " + limit + " " + sort;
 	}
-	
-	@GetMapping(path="/{userId}", produces = { MediaType.APPLICATION_XML_VALUE,
-			MediaType.APPLICATION_JSON_VALUE })
+
+	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		UserRest returnValue = new UserRest();
 		returnValue.setEmail("test@test.com");
 		returnValue.setFirstName("DJ");
 		returnValue.setLastName("Lee");
-		return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
-	
-	@PostMapping
-	public String createUser() {
-		return "User was created!";
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
+			produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
-	
+
 	@PutMapping
 	public String updateUser() {
 		return "Update user!";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 		return "Delete user!";
