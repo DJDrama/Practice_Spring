c2c90cfa21386b5148babd03facbecbe9a321aa7
DJDrama <putzzang@naver.com>
2021-02-03 16:01:29 +0900

    Autowired, Service Dependency Injection

--- mobile-app-ws/src/main/java/com/dj/practice/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/dj/practice/ui/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +24,17 @@ import com.dj.practice.ui.exceptions.UserServiceException;
 import com.dj.practice.ui.model.request.UpdateUserDetailsRequestModel;
 import com.dj.practice.ui.model.request.UserDetailsRequestModel;
 import com.dj.practice.ui.model.response.UserRest;
+import com.dj.practice.userservice.UserService;
+import com.dj.practice.userservice.impl.UserServiceImpl;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 	Map<String, UserRest> users;
-
+	
+	@Autowired //will create instance (DI)
+	UserService userService;
+	
 	@GetMapping
 	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
 			@RequestParam(value = "limit", defaultValue = "50") int limit,
@@ -39,7 +45,7 @@ public class UserController {
 	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		
-		if(true) throw new UserServiceException("User service exception thrown");
+		//if(true) throw new UserServiceException("User service exception thrown");
 		
 		if (users.containsKey(userId)) {
 			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
@@ -51,17 +57,8 @@ public class UserController {
 	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, produces = {
 			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
-		UserRest returnValue = new UserRest();
-		returnValue.setEmail(userDetails.getEmail());
-		returnValue.setFirstName(userDetails.getFirstName());
-		returnValue.setLastName(userDetails.getLastName());
-
-		String userId = UUID.randomUUID().toString();
-		returnValue.setUserId(userId);
-		if (users == null)
-			users = new HashMap<>();
-		users.put(userId, returnValue);
-
+		
+		UserRest returnValue = userService.createUser(userDetails);
 		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 
---
+++ mobile-app-ws/src/main/java/com/dj/practice/userservice/UserService.java
@@ -0,0 +1,8 @@
+package com.dj.practice.userservice;
+
+import com.dj.practice.ui.model.request.UserDetailsRequestModel;
+import com.dj.practice.ui.model.response.UserRest;
+
+public interface UserService {
+	UserRest createUser(UserDetailsRequestModel userDetails);
+}
