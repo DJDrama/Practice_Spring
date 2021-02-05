34a50d0408060c49613746670d0b7cae3f24459f
DJDrama <putzzang@naver.com>
2021-02-05 17:02:15 +0900

    UserEntity, table, column, and etc

--- Eureka_MicoService/PhotoAppApiUsers/src/main/java/com/dj/photoapp/discovery/api/users/service/UsersServiceImpl.java
+++ Eureka_MicoService/PhotoAppApiUsers/src/main/java/com/dj/photoapp/discovery/api/users/service/UsersServiceImpl.java
@@ -1,12 +1,14 @@
 package com.dj.photoapp.discovery.api.users.service;
 
+import java.util.UUID;
+
 import com.dj.photoapp.discovery.api.users.shared.UserDto;
 
 public class UsersServiceImpl implements UsersService {
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
-		
+		userDetails.setUserId(UUID.randomUUID().toString());
 		return null;
 	}
 
