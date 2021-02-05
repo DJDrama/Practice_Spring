96a356f471c1beb04ada0fbc3e04c6d32513378e
DJDrama <putzzang@naver.com>
2021-02-05 18:15:12 +0900

    Jackson xml and consume, produces both xml and json

--- Eureka_MicoService/PhotoAppApiUsers/src/main/java/com/dj/photoapp/discovery/api/users/security/WebSecurity.java
+++ Eureka_MicoService/PhotoAppApiUsers/src/main/java/com/dj/photoapp/discovery/api/users/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.dj.photoapp.discovery.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,12 +10,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	private Environment environment;
+	
+	@Autowired
+	public WebSecurity(Environment environment) {
+		this.environment = environment;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//jwt token
 		http.csrf().disable();
 		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		// only for my ip address
+		// http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
 		
 		// for h2 console
 		http.headers().frameOptions().disable();
--- Eureka_MicoService/PhotoAppApiUsers/src/main/java/com/dj/photoapp/discovery/api/users/ui/controllers/UsersController.java
+++ Eureka_MicoService/PhotoAppApiUsers/src/main/java/com/dj/photoapp/discovery/api/users/ui/controllers/UsersController.java
@@ -7,6 +7,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dj.photoapp.discovery.api.users.data.UserEntity;
 import com.dj.photoapp.discovery.api.users.service.UsersService;
 import com.dj.photoapp.discovery.api.users.shared.UserDto;
 import com.dj.photoapp.discovery.api.users.ui.model.CreateUserRequestModel;
@@ -26,26 +26,25 @@ public class UsersController {
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	UsersService usersService;
-	
 
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port " + env.getProperty("local.server.port");
 	}
-	
 
-	@PostMapping
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-		
+
 		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
-		
+
 		UserDto createdUser = usersService.createUser(userDto);
-		
+
 		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
 		return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
 	}
